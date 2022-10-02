872e6d9f399f763782af5f26d0bf818aa39a7ff0
Chen <lerx98@gmail.com>
2022-10-02 12:44:46 +0800

    enhance UI of select and matching page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -13,7 +13,7 @@ function HomePage() {
             <Typography variant="h2" color="inherit" component="div">
                 Welcome to PeerPrep.
             </Typography>
-
+            <br></br>
             <Button variant={"outlined"} onClick={handleFindMatch}>
                 Find a match!
             </Button>
--- frontend/src/pages/SelectDiffcultyPage.js
+++ frontend/src/pages/SelectDiffcultyPage.js
@@ -1,60 +1,70 @@
-import { Button, Container, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 
 const SelectDiffcultyPage = () => {
-  const { user, setUser } = useContext(UserContext);
-  const navigate = useNavigate();
+    const { user, setUser } = useContext(UserContext);
+    const navigate = useNavigate();
 
-  // For testing
-  useEffect(() => {
-    if (!user) {
-      setUser({ username: "david", hasSelectedDifficulty: false, difficultyLevel: null });
-    }
-  });
-
-  const clickDifficulty = (e) => {
-    console.log(e.target.id);
-    setUser((prevState) => {
-      return {
-        ...prevState,
-        hasSelectedDifficulty: true,
-        difficultyLevel: e.target.value,
-      };
+    // For testing
+    useEffect(() => {
+        if (!user) {
+            setUser({ username: "david", hasSelectedDifficulty: false, difficultyLevel: null });
+        }
     });
-    navigate("/matching");
-  };
 
-  return (
-    <Container>
-      <Typography variant="h1">Select Difficulty</Typography>
-      <Button
-        id="btn-easy"
-        value="easy"
-        variant="outlined"
-        onClick={clickDifficulty}
-      >
-        Easy
-      </Button>
-      <Button
-        id="btn-medium"
-        value="medium"
-        variant="outlined"
-        onClick={clickDifficulty}
-      >
-        Medium
-      </Button>
-      <Button
-        id="btn-hard"
-        value="hard"
-        variant="outlined"
-        onClick={clickDifficulty}
-      >
-        Hard
-      </Button>
-    </Container>
-  );
+    const clickDifficulty = (e) => {
+        console.log(e.target.id);
+        setUser((prevState) => {
+            return {
+                ...prevState,
+                hasSelectedDifficulty: true,
+                difficultyLevel: e.target.value,
+            };
+        });
+        navigate("/matching");
+    };
+
+    return (
+        <Stack padding="5%">
+            <Typography variant="h2">Select Difficulty</Typography>
+            <br></br>
+            <Button
+                id="btn-easy"
+                value="easy"
+                color="success"
+                size="large"
+                variant="outlined"
+                onClick={clickDifficulty}
+            >
+                Easy
+            </Button>
+            <br></br>
+            <br></br>
+            <Button
+                id="btn-medium"
+                value="medium"
+                size="large"
+                variant="outlined"
+                onClick={clickDifficulty}
+            >
+                Medium
+            </Button>
+            <br></br>
+            <br></br>
+            <Button
+                id="btn-hard"
+                value="hard"
+                color="error"
+                size="large"
+                variant="outlined"
+                onClick={clickDifficulty}
+            >
+                Hard
+            </Button>
+        </Stack>
+    );
 };
 
 export default SelectDiffcultyPage;
