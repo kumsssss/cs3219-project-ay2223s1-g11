f6627ff948d54f3430c252fed5268b0e88822e03
kumsssss <kumaran.selvvaratnam@gmail.com>
2022-11-01 16:41:47 +0800

    update user-service

--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 
 //Set up mongoose connection
 import mongoose from 'mongoose';
-
+console.log(process.env)
 let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
